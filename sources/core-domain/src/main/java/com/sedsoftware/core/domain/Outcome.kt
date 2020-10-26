0a2cc0ff44552a48e4362c756999406f4072c29c
DJ_Kovrik <djkovrik@gmail.com>
2020-10-26 19:12:23 +0300

    The module itself

---
+++ sources/core-domain/src/main/java/com/sedsoftware/core/domain/Outcome.kt
@@ -0,0 +1,14 @@
+package com.sedsoftware.core.domain
+
+sealed class Outcome<out T : Any> {
+
+    data class Success<out T : Any>(val data: T) : Outcome<T>()
+    data class Error(val throwable: Throwable) : Outcome<Nothing>()
+
+    override fun toString(): String {
+        return when (this) {
+            is Success<*> -> "Success[data=$data]"
+            is Error -> "Error[exception=$throwable]"
+        }
+    }
+}
