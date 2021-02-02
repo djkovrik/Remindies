69e6513e54c7cf90ea1b8d516f8b6030f2313bdd
DJ_Kovrik <djkovrik@gmail.com>
2021-02-02 19:30:24 +0300

    Fix build

--- common/domain/build.gradle.kts
+++ common/domain/build.gradle.kts
@@ -8,8 +8,11 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
+                implementation(Deps.Core.MVIKotlin.rx)
                 implementation(Deps.Core.MVIKotlin.mvikotlin)
                 implementation(Deps.Core.MVIKotlin.mvikotlinExtensionsReaktive)
+                implementation(Deps.Core.Decompose.decompose)
+                implementation(Deps.Core.Decompose.extensionsCompose)
                 implementation(Deps.Core.Reaktive.reaktive)
             }
         }
